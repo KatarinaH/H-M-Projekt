ad0b8bcc6b396d385a6d0d16cc791886ff9c4c7c
KatarinaH <katarina.hansson11@gmail.com>
2015-06-02 10:19:45 +0200

    blue bg color

--- index.php
+++ index.php
@@ -3,8 +3,11 @@
 <head>
 	<title></title>
 	<meta charset="utf-8">
-	<link rel="stylesheet" href="css/style.css">
+	<meta name="viewport" content="width=device-width; initial-scale=1">
 	<meta name="description" content="Välkommen till H&M, din shoppingdestination för kläder online. Shoppa de senaste trenderna och upptäck mode och kvalitet till bästa pris.">
+	
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 </head>
 <body>
 
