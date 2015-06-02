9c483d82850b4bf8df2f947759620922247489c3
frittie <frida.svensson@telia.com>
2015-06-02 10:43:31 +0200

    menu

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width; initial-scale=1">
 	<meta name="description" content="Välkommen till H&M, din shoppingdestination för kläder online. Shoppa de senaste trenderna och upptäck mode och kvalitet till bästa pris.">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	<link href='http://fonts.googleapis.com/css?family=Lora:400,700' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="css/style.css">
@@ -13,7 +14,7 @@
 <body>
 	<div id="menu-top">
 		<ul>
-			<li>shoppingbag</li>
+			<li><i class="fa fa-shopping-cart"></i> shoppingbag</li>
 			<li>kassa</li>
 		</ul>
 	</div>
