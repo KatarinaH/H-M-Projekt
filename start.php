ac34d9842c511204f0a3012e7c041347d89c006b
KatarinaH <katarina.hansson11@gmail.com>
2015-06-02 17:48:10 +0200

    start.php

---
+++ start.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width; initial-scale=1">
+	<meta name="description" content="Välkommen till H&M, din shoppingdestination för kläder online. Shoppa de senaste trenderna och upptäck mode och kvalitet till bästa pris.">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<link href='http://fonts.googleapis.com/css?family=Lora:400,700' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+</head>
+<body>
+
+<div class="start-left" id="up-left">
+	<div class="shop textarea">
+		<a href="index.php"><span>SHOP</span></a>
+	</div>
+</div>
+<div class="start-right" id="up-right">
+	
+</div>
+<div class="start-left" id="down-left">
+	
+</div>
+<div class="start-right" id="down-right">
+	<div class="info textarea">
+		<a href="#"><span>INFO</span></a>
+	</div>
+</div>
+
+<script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>	
+<script src="js/main.js"></script>
+</body>
+</html>
