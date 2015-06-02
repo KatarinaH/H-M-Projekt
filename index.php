4b3c6a5c25a742dd67554b49ff661d5356f809d3
KatarinaH <katarina.hansson11@gmail.com>
2015-06-02 10:16:25 +0200

    index.php fix

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/style.css">
+	<meta name="description" content="Välkommen till H&M, din shoppingdestination för kläder online. Shoppa de senaste trenderna och upptäck mode och kvalitet till bästa pris.">
+</head>
+<body>
+
+
+<script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>	
+<script src="js/main.js"></script>
+</body>
+</html>
