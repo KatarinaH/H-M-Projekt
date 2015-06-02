1c274b98a2c85b4f89d773b1e7979b9c7c8341ee
frittie <frida.svensson@telia.com>
2015-06-02 10:24:56 +0200

    fonts

--- index.php
+++ index.php
@@ -5,11 +5,14 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width; initial-scale=1">
 	<meta name="description" content="Välkommen till H&M, din shoppingdestination för kläder online. Shoppa de senaste trenderna och upptäck mode och kvalitet till bästa pris.">
-	
+	<link href='http://fonts.googleapis.com/css?family=Lora:400,700' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="css/style.css">
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 </head>
 <body>
+<h1>hej</h1>
+<p>hejsan</p>
 
 
 <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>	
