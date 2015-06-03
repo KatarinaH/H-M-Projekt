a2663f83c327c0c18819ba029dbf7a13e93d1bff
frittie <frida.svensson@telia.com>
2015-06-03 13:50:17 +0200

    hjölp

--- info.php
+++ info.php
@@ -2,7 +2,10 @@
 
 	<div id="info-wrapper">
 		<div class="block3 grid-item">
-			<iframe width="100%" height="100%" src="https://www.youtube.com/embed/cfcJVC_pnOk?autoplay=0&controls=0&showinfo=0&rel=0" frameborder="0" allowfullscreen></iframe>
+
+			<!--<iframe width="670px" height="380px" src="https://www.youtube.com/embed/edP4Sa_F8eY?rel=0&amp;controls=0&amp;showinfo=0&rel=0" frameborder="0" allowfullscreen></iframe>-->
+
+			<iframe width="670px" height="380px" src="https://www.youtube.com/embed/cfcJVC_pnOk?autoplay=0&controls=0&showinfo=0&rel=0" frameborder="0" allowfullscreen></iframe>
 		</div>
 		<div class="block1 grid-item">
 			<div class="block2 box3"></div>
