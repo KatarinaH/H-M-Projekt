941f7144a5bed00c6c3e54d15ec9957ecd3f06b3
frittie <frida.svensson@telia.com>
2015-06-03 12:01:16 +0200

    info.php

--- info.php
+++ info.php
@@ -9,20 +9,27 @@
 <body>
 
 	<div id="info-wrapper">
-		<div class="block2 grid-item"></div>
-		<div class="block2 grid-item"></div>
-		<div class="block1 grid-item"></div>
-		<div class="block1 grid-item"></div>
+		<div></div>
+		<!--<div class="block1 grid-item box1"></div>
+		<div class="block1 grid-item box2"></div>-->
+		<div class="block1 grid-item">
+			<div class="block2 box3"></div>
+			<div class="block2 box4"></div>
+		</div>
+		
+		<div class="block1 grid-item">
+			<div class="block2 box5"></div>
+			<div class="block2 box6"></div>
+		</div>
+		<div class="block1 grid-item box7"></div>
+		<div class="block1 grid-item box8"></div>
 
-		<div class="block1 grid-item"></div>
-		<div class="block1 grid-item"></div>
-		<div class="block2 grid-item"></div>
-		<div class="block2 grid-item"></div>
-
-		<div class="block2 grid-item"></div>
-		<div class="block1 grid-item"></div>
-		<div class="block1 grid-item"></div>
-		<div class="block2 grid-item"></div>
+		<div class="block1 grid-item box9"></div>
+		<div class="block1 grid-item">
+			<div class="block2 box10"></div>
+			<div class="block2 box11"></div>
+		</div>
+		<div class="block1 grid-item box12"></div>
 	</div>
 
 <?php
