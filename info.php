b59791a2c62ba627a61b2418817a5346fb045606
KatarinaH <katarina.hansson11@gmail.com>
2015-06-03 11:03:24 +0200

    header.php

--- info.php
+++ info.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Info</title>
-	<meta charset="utf-8"/>
-	<link rel="stylesheet" type="text/css" href="css/style.css" />
-	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-</head>
-<body>
+<?php include 'header.php'; ?>
 
 	<div id="info-wrapper">
 		<div class="block2 grid-item"></div>
---
+++ menu-shop.php
@@ -0,0 +1,7 @@
+<div id="menu-top">
+	<img class="logo" src="img/h&m.png" alt="H&M Logo">
+	<ul>
+		<li><img src="img/shopping_bag.png" alt="Shopping Bag"> shoppingbag</li>
+		<li>kassa</li>
+	</ul>
+</div>
