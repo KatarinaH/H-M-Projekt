3f4d2ecf6f959487b9f55e93239cd4e16d23491b
frittie <frida.svensson@telia.com>
2015-06-03 15:47:14 +0200

    changed header.php

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
 	<title></title>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width; initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="Välkommen till H&M, din shoppingdestination för kläder online. Shoppa de senaste trenderna och upptäck mode och kvalitet till bästa pris.">
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	<link href='http://fonts.googleapis.com/css?family=Lora:400,700' rel='stylesheet' type='text/css'>
